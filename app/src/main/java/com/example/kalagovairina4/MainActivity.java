b11b44bc73582a6c8245c3d39ee57fb49cdef45d
госпожа <kalagova.irina@gmail.com>
2024-03-18 03:13:57 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kalagovairina4/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.kalagovairina4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+//
+//        // Создаем объект FrameLayout
+//        FrameLayout frameLayout = new FrameLayout(this);
+//        frameLayout.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT));
+//        setContentView(frameLayout);
+//
+//        // Создаем кнопку
+//        Button button = new Button(this);
+//        button.setText("кнопочка посерединке");
+//        FrameLayout.LayoutParams buttonParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+//        buttonParams.gravity = Gravity.CENTER; // Устанавливаем расположение по центру
+//        button.setLayoutParams(buttonParams);
+//        frameLayout.addView(button);
+
+
+
+//        // Создаем объект ScrollView
+//        ScrollView scrollView = new ScrollView(this);
+//        LinearLayout linearLayout = new LinearLayout(this);
+//        linearLayout.setOrientation(LinearLayout.VERTICAL);
+//
+//        for (int i = 0; i < 10; i++) {
+//            TextView textView = new TextView(this);
+//            textView.setText("Вкусняшка для кыти " + (i+1));
+//            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+//            linearLayout.addView(textView, layoutParams);
+//        }
+//
+//        scrollView.addView(linearLayout);
+//        setContentView(scrollView);
+
+    }
+
+
+
+    }
